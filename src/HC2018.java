0c57e6d4f39c29cb6a7c4dd47a5dcfa55f66ae23
Guilherme Azevedo <32509438+gssao-iscteiul@users.noreply.github.com>
2018-03-01 17:38:01 +0000

    .

--- src/HC2018.java
+++ src/HC2018.java
@@ -6,35 +6,35 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class HC2018 {
-	private static final String FILE_NAME = null; // TODO mudar
+    private static final String FILE_NAME = null; // TODO mudar
+    private static final String new_line = System.getProperty("line.separator");
 
-	public void readFile(File file) throws FileNotFoundException {
-		Scanner scn = new Scanner(file);
-		for (int line_index = 0; scn.hasNextLine(); line_index++) {
-			//TODO
-		}
-		
-		scn.close();
+    public void readFile(File file) throws FileNotFoundException {
+	Scanner scn = new Scanner(file);
+	for (int line_index = 0; scn.hasNextLine(); line_index++) {
+	    // TODO
 	}
-	
-	public void escreveFicheiro() throws IOException {
-		String s = "";
-		String new_line = System.getProperty("line.separator");
 
-		// TODO
-		
-		File file = new File("resultado_" + FILE_NAME + ".in");
-		file.createNewFile();
-		try {
-			BufferedWriter out = new BufferedWriter(new FileWriter(file.getAbsolutePath()));
-			out.write(s);
-			out.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+	scn.close();
+    }
+
+    public void escreveFicheiro() throws IOException {
+	String s = "";
+
+	// TODO
+
+	File file = new File("resultado_" + FILE_NAME + ".in");
+	file.createNewFile();
+	try {
+	    BufferedWriter out = new BufferedWriter(new FileWriter(file.getAbsolutePath()));
+	    out.write(s);
+	    out.close();
+	} catch (Exception e) {
+	    System.out.println(e);
 	}
-	
-	public static void main(String[] args) {
-		
-	}
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
